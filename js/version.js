7ed0d199db45135eea551877d7d434a8af6869f7
Jack W. Parks <discerningowl@users.noreply.github.com>
2026-08-16 12:57:20 -0400

    Update WX4PTC page
    updated some of the content on the WX4PTC page

--- js/version.js
+++ js/version.js
@@ -15,7 +15,7 @@
   // ========================================================================
   // UPDATE THIS VERSION NUMBER TO FORCE CACHE REFRESH ACROSS ENTIRE SITE
   // ========================================================================
-  const APP_VERSION = '20260804o';
+  const APP_VERSION = '20260816a';
 
   // Export to global namespace
   window.APP_VERSION = APP_VERSION;
